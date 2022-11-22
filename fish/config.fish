6ade8503014834102a308e4db062e03be6a2ff70
shljsl75891 <shljsl75891@gmail.com>
2022-11-22 21:18:34 +0530

    updated

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	export PATH="$HOME/.emacs.d/bin:$PATH"
     alias ls='exa -alh --color=always --group-directories-first' 
 	alias clear='clear && pfetch'
     alias mkdir='mkdir -pv' 
 	alias rm='rm -v'
-	alias g++='g++ -std=c++20 -Wall'
-    alias search='nala search'
-    alias install='sudo nala install'
-    alias purge='sudo nala purge'
-    alias history='sudo nala history'
-	alias emacs='emacsclient -c -a "nvim"'
     starship init fish | source
 	pfetch
 end
